e0e074a0e5f78f8544abc2f986510f8efc64192a
jebus_perfidia@outlook.com <jesus.eduardo.camargo@outlook.com>
2023-01-05 00:10:40 -0600

    Corrección de problema stock menor

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -8,6 +8,7 @@ use App\Models\Ticket;
 use App\Models\TicketDetail;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Response;
 use Validator;
 
 class TicketController extends Controller
@@ -108,9 +109,10 @@ class TicketController extends Controller
             foreach ($sk as $key => $value) {
 
                 //Obtenemos el producto, a partir del código de barras, para obtener el total de kilos a comparar
-                $product = Product::where('barcode', $value['barcode'])->first();
+                //$product = Product::where('barcode', $value['barcode'])->first();
+                $product = Product::find($value['product_id']);
                 //Primero validamos si hay stock en el inventario
-                
+                //dd($product);
                 if($product) {
                     if ($product->stock_kilos === 0.0) {
                         array_push(
@@ -207,10 +209,15 @@ class TicketController extends Controller
 
             //Si existió algún problema con el stock de alguno de los productos, enviamos una respuesta en formato JSON
             if (count($stocksValidation) > 0) {
-                return response()->json([
+
+                json_encode($stocksValidation);
+
+                return Response::make($stocksValidation, 404);
+
+                /* return response()->json([
                     "status" => false,
                     "errors" => $stocksValidation
-                ], 404);
+                ], 404); */
             }
 
         }
